f5976bd59a434b595171f20ae1196c939173e185
Andrei Serebro <andy.silv@yandex.ru>
2025-11-12 00:49:49 +0000

    run bot app inside docker compose

--- cycleparks/analytics.py
+++ cycleparks/analytics.py
@@ -5,7 +5,7 @@ import redis.asyncio as redis
 from dataclasses import astuple
 from datetime import datetime
 
-r = redis.Redis()
+r = redis.Redis(host="redis")
 logger = logging.getLogger(__name__)
 
 DUMP_FREQUENCY = 10  # seconds, how often to flush logs to the database
@@ -28,37 +28,40 @@ async def log_send_failure(msg_type, error_message):
     await r.expire(key, 86400)  # 24 hours
 
 
-async def _flush_failures_to_postgres(db_pool: asyncpg.Pool, error_queue: asyncio.Queue):
+async def _flush_failures_to_postgres(
+    db_pool: asyncpg.Pool, error_queue: asyncio.Queue
+):
     keys = await r.keys("failures|*")
     async with db_pool.acquire() as conn:
-      results = []
-      for key in keys:
-          count = int(await r.get(key))
-          # Parse key: failures|2025-06-04-17:30|photo|RateLimitExceeded
-          parts = key.decode().split("|")
-          timestamp = datetime.strptime(parts[1], SEND_FAILURE_TIME_FORMAT)
-          msg_type = parts[2]
-          error = parts[3]
-          results.append((timestamp, msg_type, error, count))
-          await r.delete(key)
-      logger.info("Flushing %d send failures to Postgres", len(results))
-      await conn.executemany(
-          "INSERT INTO send_failures (timestamp, message_type, error_message, count) VALUES ($1, $2, $3, $4)",
-          results
-      )
+        results = []
+        for key in keys:
+            count = int(await r.get(key))
+            # Parse key: failures|2025-06-04-17:30|photo|RateLimitExceeded
+            parts = key.decode().split("|")
+            timestamp = datetime.strptime(parts[1], SEND_FAILURE_TIME_FORMAT)
+            msg_type = parts[2]
+            error = parts[3]
+            results.append((timestamp, msg_type, error, count))
+            await r.delete(key)
+        logger.info("Flushing %d send failures to Postgres", len(results))
+        await conn.executemany(
+            "INSERT INTO send_failures (timestamp, message_type, error_message, count) VALUES ($1, $2, $3, $4)",
+            results,
+        )
 
-      results = []
-      while not error_queue.empty():
-          try:
-              item = error_queue.get_nowait()
-              results.append(astuple(item))
-          except asyncio.QueueEmpty:
-              break
-      logger.info("Flushing %d unhandled errors to Postgres", len(results))
-      await conn.executemany(
-          "INSERT INTO errors (timestamp, exception_type, error_message, update_str) VALUES ($1, $2, $3, $4)",
-          results
-      )
+        results = []
+        while not error_queue.empty():
+            try:
+                item = error_queue.get_nowait()
+                results.append(astuple(item))
+            except asyncio.QueueEmpty:
+                break
+        logger.info("Flushing %d unhandled errors to Postgres", len(results))
+        await conn.executemany(
+            "INSERT INTO errors (timestamp, exception_type, error_message, update_str) VALUES ($1, $2, $3, $4)",
+            results,
+        )
+
 
 async def flush_failures_to_postgres(db_pool: asyncpg.Pool, error_queue: asyncio.Queue):
     while True:
@@ -80,5 +83,7 @@ async def flush_logs(db_pool: asyncpg.Pool):
             ts = datetime.strptime(ts, TIME_FORMAT)
             await conn.execute(
                 "INSERT INTO requests (timestamp, user_id, command) VALUES ($1, $2, $3)",
-                ts, int(user_id), cmd)
-
+                ts,
+                int(user_id),
+                cmd,
+            )
